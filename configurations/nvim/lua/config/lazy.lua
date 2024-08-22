e9effe2f50dced60425cfd4efaf79ab82d7c79f6
M.Abdulrahman Alnaseer <20760062+abdalrohman@users.noreply.github.com>
2024-08-23 00:48:50 +0300

    Fix: Removed ui-edgy plugin due to scroll glitches when opening files beside terminal tabs.

--- configurations/nvim/lua/config/lazy.lua
+++ configurations/nvim/lua/config/lazy.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
+    -- { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
